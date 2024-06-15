6612229ea444a56441722a11682965f1feee3958
Ing. Ricardo Elizalde <ricardo.elizaldeh@un.edu.mx>
2024-06-15 11:50:57 -0600

    Codigo inicial

---
+++ ManejoBasicoDeGit/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ManejoBasicoDeGit.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
